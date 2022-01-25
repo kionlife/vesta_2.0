50abac33a67a12313db3f225d6f83cfc33f9fb04
SK0Rua <86987756+SK0Rua@users.noreply.github.com>
2022-01-26 01:13:52 +0200

    family

---
+++ database/migrations/2022_01_19_151756_create_tariffs_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTariffsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tariffs', function (Blueprint $table) {
+            $table->integer('id')->primary();
+            $table->string('name');
+            $table->float('value');
+            $table->integer('city_id')->nullable();
+            $table->integer('service_id');
+            $table->integer('provider_id')->default(1);
+            $table->integer('abonent_type')->default(0);
+            $table->integer('virtual')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tariffs');
+    }
+}
